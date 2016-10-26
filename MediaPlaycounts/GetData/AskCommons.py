cafcec0461516086e431d94d63f26d22fee9da96
harej <jamesmhare@gmail.com>
2016-10-26 12:52:27 -0400

    Working

--- MediaPlaycounts/GetData/AskCommons.py
+++ MediaPlaycounts/GetData/AskCommons.py
@@ -54,7 +54,6 @@ def find_subcategories(category, depth=9, db="commonswiki_p",
                            host=host, read_default_file=read_default_file, port=port,
                            success_log=success_log, error_log=error_log)
         for more_result in more:
-            more_val = more_result[0]
             categorylist.append(more_val)
 
     return list(set(categorylist))
