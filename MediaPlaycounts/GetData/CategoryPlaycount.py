7bdad72a203175dfcd96328869d1b743b8437001
harej <jamesmhare@gmail.com>
2016-10-31 16:31:07 -0400

    Including filename in the API output for category-related lookups

--- MediaPlaycounts/GetData/CategoryPlaycount.py
+++ MediaPlaycounts/GetData/CategoryPlaycount.py
@@ -61,7 +61,7 @@ def date(category, date, depth=9, db="s53189__mediaplaycounts_p",
         subquery = FilePlaycount.date(filename, date, db=db,
                        read_default_file=read_default_file, host=host, port=port,
                        success_log=success_log, error_log=error_log)
-        output.append(subquery[0])
+        output.append({"filename": filename, "details": subquery[0]})
     for triplet in output:
         total += triplet["count"]
 
