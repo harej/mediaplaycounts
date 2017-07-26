2e5cb10e26dcf74dcd50545d6849b5bde8f1cdfb
harej <jamesmhare@gmail.com>
2017-07-25 22:26:57 -0700

    Adding method to delete file after it is done being used

--- MediaPlaycounts/LogProcessor.py
+++ MediaPlaycounts/LogProcessor.py
@@ -1,43 +1,8 @@
-import arrow, bz2, os, re, requests, sys, urllib.parse
+import arrow, bz2, hashlib, os, re, requests, sys, urllib.parse
 from helper import Helper
 
 h = Helper()
 DATE_REGEX = re.compile('^\d{8}$')
-
-
-def parse(row):
-    """
-    Takes a line from a raw, decompressed log file and returns a tuple
-    (filename, playcount)
-    """
-
-    ext_regex = re.compile('.*\.(mid|ogg|ogv|wav|webm|flac|oga)')
-
-    columns = row.split('\t')
-    if len(columns) < 2:  # Not a real row
-        return
-    base_name = columns[0]
-
-    if columns[3] == '-':
-        columns[3] = 0
-    if columns[4] == '-':
-        columns[4] = 0
-    if columns[16] == '-':
-        columns[16] = 0
-
-    playcount = int(columns[3]) + int(columns[4]) + int(columns[16])
-
-    if playcount > 0:
-        # First we must determine if this is a media file
-        components = base_name.split('/')
-
-        # /wikipedia/commons/x/xx/FILENAME
-        if len(components) == 6:
-            if components[1] == 'wikipedia' and components[2] == 'commons':
-                if len(components[3]) == 1 and len(components[4]) == 2:
-                    filename = urllib.parse.unquote_plus(components[5])
-                    if re.match(ext_regex, filename) is not None:
-                        return (filename, int(playcount))
 
 
 def download(date):
@@ -70,19 +35,61 @@ def download(date):
     h.success_log('Processed ' + to_download)
 
 
-def store(record, date):
+def parse(row):
     """
-    Takes the output of the parse function (the output here being referred to as
-    `record`) as well as the Arrow object representing the date of the log and
-    stores it in Redis. Returns True on success. Raises an exception upon failure.
+    Takes a line from a raw, decompressed log file and returns a tuple
+    (filename, playcount)
     """
 
-    date_string = date.format('YYYYMMDD')
+    video_regex = re.compile('.*\.(mid|ogg|ogv|wav|webm|flac|oga)')
 
-    for pair in record:
-        h.redis.hincrby('mpc:' + pair[0], date_string, amount=pair[1])
+    columns = row.split('\t')
+    if len(columns) < 2:  # Not a real row
+        return
+    base_name = columns[0]
 
-    return True
+    for column_num, column in enumerate(columns):
+        if column == '-':
+            columns[column_num] = 0
+        elif column_num > 0:  # column 0 is the name row
+            columns[column_num] = int(column)
+
+    original = columns[3]
+    playable_transcoded = columns[4] + columns[16]
+    thumbnails = {
+        '0-399': columns[8] + columns[9],
+        '400-799': columns[10] + columns[11],
+        '800': columns[12] + columns[13]
+    }
+
+    if columns[3] + columns[4] + columns[7] + columns[16] > 0:
+        # First we must determine if this is a media file
+        components = base_name.split('/')
+        # /wikipedia/commons/x/xx/FILENAME
+        if len(components) == 6:
+            if components[1] == 'wikipedia' and components[2] == 'commons':
+                if len(components[3]) == 1 and len(components[4]) == 2:
+                    filename = urllib.parse.unquote_plus(components[5])
+                    if re.match(video_regex, filename) is not None:
+                        return (filename, original, playable_transcoded,
+                                'playable')
+                    else:
+                        return (filename, original, thumbnails, 'static')
+
+
+def store(engine, prefix, filename, date, payload):
+    """
+    Stores a given `payload` in the `date` field of the `prefix`:`filename`
+    hash table in Redis.
+    """
+
+    if engine == 'redis':
+        h.redis.hincrby(prefix + filename, date, amount=payload)
+    elif engine == 'ssdb':
+        # We change the filename to a hash because SSDB is inexplicably fussy
+        # about the keys you use.
+        filename = hashlib.sha224(filename.encode('utf-8')).hexdigest()
+        h.ssdb.hincr(prefix + filename, date, payload)
 
 
 def delete_file(date):
@@ -104,12 +111,36 @@ def run(dates=[arrow.utcnow().replace(days=-1)]):
 
     for date in dates:
         record = []
-        date_string = date.format('YYYY-MM-DD')
+        date_string = date.format('YYYYMMDD')
         print('Processing: ' + date_string)
         for line in download(date):
             line = parse(line)
             if line is not None:
-                store([line], date)
+                filename, originals, transcodes, filetype = line
+                if filetype == 'playable':
+                    if originals + transcodes > 0:
+                        store('redis', 'mpc:', filename, date_string,
+                              originals + transcodes)
+                elif filetype == 'static':
+                    # We append an integer to the date string to indicate which
+                    # cohort of loads. This is because it can be stored as an
+                    # integer, saving precious memory.
+                    # 0 = originals
+                    # 1 = 0-399
+                    # 2 = 400-799
+                    # 3 = 800+
+                    if originals > 0:
+                        store('ssdb', 'img:', filename, date_string + '0',
+                              originals)
+                    for interval, val in transcodes.items():
+                        if val > 0:
+                            if interval == '0-399':
+                                interval_code = date_string + '1'
+                            elif interval == '400-799':
+                                interval_code = date_string + '2'
+                            elif interval == '800':
+                                interval_code = date_string + '3'
+                            store('ssdb', 'img:', filename, interval_code, val)
         delete_file(date)
 
 
